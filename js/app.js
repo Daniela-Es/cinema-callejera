513c61a9d268fad48f2488d737c3f0d18662d518
Daniela Estrada <dannydonnytas@gmail.co>
2021-03-31 11:00:42 -0600

    primer commit

---
+++ js/app.js
@@ -0,0 +1,100 @@
+console.log("todo bien");
+
+function dosdeabril() {
+  Swal.fire({
+    title: 'Pelicula 1',
+    text: 'Informacion sobre la pelicula 1',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula uno',
+  })
+}
+
+function nuevedeabril() {
+  Swal.fire({
+    title: 'Pelicula 2',
+    text: 'Informacion sobre la pelicula 2',
+    imageUrl: 'http://placeimg.com/640/480/business',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula dos',
+  })
+}
+
+function diesciseisdeabril() {
+  Swal.fire({
+    title: 'Pelicula 3',
+    text: 'Informacion sobre la pelicula 3',
+    imageUrl: 'http://placeimg.com/640/480/catsess',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula tres',
+  })
+}
+
+function veintitresdeabril() {
+  Swal.fire({
+    title: 'Pelicula 4',
+    text: 'Informacion sobre la pelicula 4',
+    imageUrl: 'http://placeimg.com/640/480/abstract',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula cuatro',
+  })
+}
+
+function treintadeabril() {
+  Swal.fire({
+    title: 'Pelicula 5',
+    text: 'Informacion sobre la pelicula 5',
+    imageUrl: 'http://placeimg.com/640/480/food/640/480/abstract',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula cinco',
+  })
+}
+
+function sietedemayo() {
+  Swal.fire({
+    title: 'Pelicula 6',
+    text: 'Informacion sobre la pelicula 6',
+    imageUrl: 'http://placeimg.com/640/480/people',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula seis',
+  })
+}
+
+function catorcedemayo() {
+  Swal.fire({
+    title: 'Pelicula 7',
+    text: 'Informacion sobre la pelicula 7',
+    imageUrl: 'http://placeimg.com/640/480/animals',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula siete',
+  })
+}
+
+function veintiunodemayo() {
+  Swal.fire({
+    title: 'Pelicula 8',
+    text: 'Informacion sobre la pelicula 8',
+    imageUrl: 'http://placeimg.com/640/480/nightlife',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula ocho',
+  })
+}
+
+function veintiochodemayo() {
+  Swal.fire({
+    title: 'Pelicula 9',
+    text: 'Informacion sobre la pelicula 9',
+    imageUrl: 'http://placeimg.com/640/480/nature',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'pelicula nueve',
+  })
+}
